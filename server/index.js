7b70fdf5a804e0bb5c897584168617b60819ce42
Saharsh Samir <saharshsamir10@gmail.com>
2022-03-26 19:45:47 +0530

    started razorpay integration

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 require("./db/conn");
 app.use("/", require("./routes/UserRoutes"));
-
+app.use("/reques", require("./routes/RequestRoutes"));
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
