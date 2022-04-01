4df7cbe6cb8f836dbe271ef172a97f80d2a74aa6
shivam256 <shivamgavandi256@gmail.com>
2022-04-01 20:26:32 +0530

    client changes

--- client/src/pages/hostProfile/HostProfile.component.jsx
+++ client/src/pages/hostProfile/HostProfile.component.jsx
@@ -7,18 +7,17 @@ import VerifiedUserIcon from "@mui/icons-material/VerifiedUser";
 import HostFormModal from "./HostFormModal";
 import useHosts from "../../hooks/useHosts";
 import useAuth from "../../hooks/useAuth";
-import {Box} from '@chakra-ui/react'
-
+import { Box } from "@chakra-ui/react";
 
 const HostProfile = () => {
   const { user } = useAuth();
   const { getSingleHost } = useHosts();
   const [User, setUser] = useState(null);
-  const [requested,setRequested] = useState(false)
+  const [requested, setRequested] = useState(false);
   const id = window.location.pathname.split("/")[2];
   useEffect(() => {
-    let isBooked = user.userRequest.some(u => u.hostId == id);
-    setRequested(isBooked)
+    let isBooked = user.userRequest.some((u) => u.hostId == id);
+    setRequested(isBooked);
     getSingleHost(id).then((res) => setUser(res));
   }, [id]);
 
@@ -27,7 +26,12 @@ const HostProfile = () => {
   };
   const [state, setState] = useState(false);
   return (
-    <Box borderRadius={"10px"} boxShadow={"2px 2px 10px #D3D3D3"} margin={"5%"} p={"0.5rem"}>
+    <Box
+      borderRadius={"10px"}
+      boxShadow={"2px 2px 10px #D3D3D3"}
+      margin={"5%"}
+      // p={"0.5rem"}
+    >
       <HostFormModal state={state} toggleModal={toggleImageModal} />
       <div className="header">
         <div className="dp-container">
@@ -50,12 +54,7 @@ const HostProfile = () => {
             </span>
           </div>
           <div className="credibility">
-            <Rating
-              name="read-only"
-              value={4}
-              readOnly
-              size="large"
-            />
+            <Rating name="read-only" value={4} readOnly size="large" />
             <span className="top-rated-span">
               <BookmarkIcon sx={{ color: "#156FAF" }} />
               Top rated
@@ -63,24 +62,25 @@ const HostProfile = () => {
           </div>
         </div>
         <div className="book-btn-container">
-        { !requested ? (
+          {!requested ? (
             <>
-            <button align="center" onClick={() => {setState(!state)
-            }} className="book-me-btn">
-            Book Me
-          </button>
+              <button
+                align="center"
+                onClick={() => {
+                  setState(!state);
+                }}
+                className="book-me-btn"
+              >
+                Book Me
+              </button>
             </>
-          ):(
+          ) : (
             <>
-            <h1 align="center">Your Request Has Been Sent!</h1>
+              <h1 align="center">Your Request Has Been Sent!</h1>
             </>
-          )
-}
+          )}
         </div>
       </div>
-
-    
-   
     </Box>
   );
 };
