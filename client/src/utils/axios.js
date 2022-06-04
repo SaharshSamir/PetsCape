553b041227ea10211cba663ec27bc8739120cfd5
AryanShinde <aryan.shinde20@vit.edu>
2022-06-04 21:13:21 +0530

    test

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://petscape-api.herokuapp.com/",
+  baseURL: "http://localhost:5000",
 });
 
 export default axiosInstance;
