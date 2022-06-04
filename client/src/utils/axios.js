f8a7318b5afa33b91dd5d7258d7a2cea734c68aa
shivam256 <shivamgavandi256@gmail.com>
2022-06-04 21:25:03 +0530

    build error fixed

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://petscape-api.herokuapp.com/",
+  baseURL: "https://petscape-api.hperokuapp.com/",
   // baseURL:"http://localhost:5000/"
 });
 
