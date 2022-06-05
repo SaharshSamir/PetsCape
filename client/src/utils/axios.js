ea68d2816281bae708effd38bfab0229f4721059
shivam256 <shivamgavandi256@gmail.com>
2022-06-06 00:32:55 +0530

    typo fixed

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://petscape-api.hperokuapp.com/",
+  baseURL: "https://petscape-api.herokuapp.com/",
   // baseURL: "http://localhost:5000/",
 });
 
